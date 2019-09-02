782d7c15c3814285e3a8577da4c0205821ea136d
Lifeifei <1032188134@qq.com>
2019-09-02 14:44:04 +0800

    修改

--- linux_start.sh
+++ linux_start.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash 
 
+echo -e "########################安装 net-tools ########################\n"
+yum install net-tools
+
+echo -e "########################安装 vim \n"
+yum install -y vim-enhanced
+
+echo "########################安装 iptables ########################\n"
+echo -e "停止 firewalld 服务 \n"
+systemctl stop firewalld.service
+
+echo -e "\n########################禁止 firewalld 服务在系统启动的时候自动启动\n"
+systemctl disable firewalld.service
+
+echo -e "\n########################安装 iptables ########################\n"
+yum install -y iptables-services
+
+echo -e "\n########################添加开机启动########################\n"
+systemctl enable iptables.service
+
+echo -e "\n########################关闭SELinux########################\n"
+echo -e "\n########################编辑前的内容：########################\n"
+cat /etc/sysconfig/selinux
+
+sed -i 's|SELINUX=enforcing|SELINUX=disabled|' /etc/sysconfig/selinux
+
+echo -e "\n########################编辑后的内容：########################\n"
+cat /etc/sysconfig/selinux
+
 echo -e '\n########################卸载旧版docker########################\n'
 
 yum remove docker \
@@ -38,8 +66,11 @@ echo '{
   "registry-mirrors": ["https://registry.docker-cn.com"]
 }'>>/etc/docker/daemon.json
 
-echo -e "\ndaemon.json内容：\n"
+echo -e "\n########################daemon.json内容：########################\n"
 cat /etc/docker/daemon.json
 
-echo -e "\n重启docker\n"
+echo -e "\n########################重启docker########################\n"
 service docker restart
+
+# echo -e "########################重启linux########################"
+# reboot
