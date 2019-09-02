599bf81ea0952ab82ebe3d85f359da9ce8f0b4c5
Lifeifei <1032188134@qq.com>
2019-09-02 14:00:33 +0800

    修改

--- linux_start.sh
+++ linux_start.sh
@@ -1,34 +1,39 @@
 #!/usr/bin/env bash 
 
-echo '########################卸载旧版docker########################/n'
+# echo '########################卸载旧版docker########################'
+# echo /n
 
-yum remove docker \
-            docker-client \
-            docker-client-latest \
-            docker-common \
-            docker-latest \
-            docker-latest-logrotate \
-            docker-logrotate \
-            docker-engine
+# yum remove docker \
+#             docker-client \
+#             docker-client-latest \
+#             docker-common \
+#             docker-latest \
+#             docker-latest-logrotate \
+#             docker-logrotate \
+#             docker-engine
 
-echo "########################安装需要的包########################/n"
-yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+# echo "########################安装需要的包########################"
+# yum install -y yum-utils \
+#   device-mapper-persistent-data \
+#   lvm2
 
-echo "########################设置仓库地址########################"
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+# echo "########################设置仓库地址########################"
+# yum-config-manager \
+#     --add-repo \
+#     https://download.docker.com/linux/centos/docker-ce.repo
 
-echo "########################安装最新版########################"
-yum install docker-ce docker-ce-cli containerd.io
+# echo "########################安装最新版########################"
+# yum install docker-ce docker-ce-cli containerd.io
 
-echo "########################启动docker"########################
-systemctl start docker
+# echo "########################启动docker########################"
+# systemctl start docker
 
-echo "########################验证docker 是否安装成功########################"
-docker run hello-world
+# echo "########################验证docker 是否安装成功########################"
+# docker run hello-world
 
-echo "########################开机自启动########################"
-systemctl enable docker
+# echo "########################开机自启动########################"
+# systemctl enable docker
+
+echo "开始"
+
+echo 'test'>>./test.txt
