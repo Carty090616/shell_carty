3d92ad59a358c9376b9fea43677386ceee8bcfd8
Lifeifei <1032188134@qq.com>
2019-09-02 13:53:23 +0800

    修改

--- linux_start.sh
+++ linux_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-echo "卸载旧版docker"
+echo '########################卸载旧版docker########################/n'
 
 yum remove docker \
             docker-client \
@@ -11,9 +11,24 @@ yum remove docker \
             docker-logrotate \
             docker-engine
 
-echo "卸载完成"
-
-echo "安装需要的包"
+echo "########################安装需要的包########################/n"
 yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
+
+echo "########################设置仓库地址########################"
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "########################安装最新版########################"
+yum install docker-ce docker-ce-cli containerd.io
+
+echo "########################启动docker"########################
+systemctl start docker
+
+echo "########################验证docker 是否安装成功########################"
+docker run hello-world
+
+echo "########################开机自启动########################"
+systemctl enable docker
