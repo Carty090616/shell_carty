78e9aaa417595c0045f9bfbdafc325d02b44a37d
Lifeifei <1032188134@qq.com>
2019-09-02 14:13:31 +0800

    修改

--- linux_start.sh
+++ linux_start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash 
 
-echo '########################卸载旧版docker########################'
-echo /n
+echo '\n########################卸载旧版docker########################\n'
 
 yum remove docker \
             docker-client \
@@ -12,31 +11,34 @@ yum remove docker \
             docker-logrotate \
             docker-engine
 
-echo "########################安装需要的包########################"
+echo "\n########################安装需要的包########################\n"
 yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
 
-echo "########################设置仓库地址########################"
+echo "\n########################设置仓库地址########################\n"
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-echo "########################安装最新版########################"
+echo "\n########################安装最新版########################\n"
 yum install docker-ce docker-ce-cli containerd.io
 
-echo "########################启动docker########################"
+echo "\n########################启动docker########################\n"
 systemctl start docker
 
-echo "########################验证docker 是否安装成功########################"
+echo "\n########################验证docker 是否安装成功########################\n"
 docker run hello-world
 
-echo "########################开机自启动########################"
+echo "\n########################开机自启动########################\n"
 systemctl enable docker
 
-echo "########################切换数据源########################"
+echo "\n########################切换数据源########################\n"
 echo '{
   "registry-mirrors": ["https://registry.docker-cn.com"]
 }'>>/etc/docker/daemon.json
+echo "\ndaemon.json内容：\n"
 cat /etc/docker/daemon.json
+
+echo "\n重启docker"
 service docker restart
